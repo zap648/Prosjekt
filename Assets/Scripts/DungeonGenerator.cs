14117233975018b41de6a61608073a410de6d829
Kenneth Massao Straumsheim <kmstraumsheim@gmail.com>
2023-11-07 15:46:28 +0100

    Procedural Generation Generation 1
    Simple procedural generation in a maze-like "blindwalking" generation from the north-westernmost corner to the south-easternmost corner.

---
+++ Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,172 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D.Aseprite;
+using UnityEngine;
+
+public class DungeonGenerator : MonoBehaviour
+{
+    public class Cell
+    {
+        public bool visited = false;
+        public bool[] status = new bool[4];
+    }
+
+    public Vector2 size;
+    public int startPos = 0;
+    public GameObject room;
+    public Vector2 offset;
+
+    List<Cell> board;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MazeGenerator();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateDungeon()
+    {
+        for (int i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j < size.y; j++)
+            {
+                Cell currentCell = board[Mathf.FloorToInt(i + j * size.x)];
+                if (currentCell.visited)
+                {
+                    var newRoom = Instantiate(room, new Vector3(i * offset.x, -j * offset.y, 0), Quaternion.identity, transform).GetComponent<RoomBehaviour>();
+                    newRoom.UpdateRoom(currentCell.status);
+
+                    newRoom.name += $" {i}-{+j}";
+                }
+            }
+        }
+    }
+
+
+    void MazeGenerator()
+    {
+        board = new List<Cell>();
+
+        for (int i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j < size.y; j++)
+            {
+                board.Add(new Cell());
+            }
+        }
+
+        int currentCell = startPos;
+
+        Stack<int> path = new Stack<int>();
+
+        int k = 0;
+
+        while (k < 1000)
+        {
+            k++;
+
+            board[currentCell].visited = true;
+
+            if (currentCell == board.Count - 1)
+            {
+                break;
+            }
+
+            // Check the cell's neighbours
+            List<int> neighbours = CheckNeighbours(currentCell);
+
+            if (neighbours.Count == 0)
+            {
+                if (path.Count == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    currentCell = path.Pop();
+                }
+            }
+            else
+            {
+                path.Push(currentCell);
+
+                int newCell = neighbours[Random.Range(0, neighbours.Count)];
+
+                if (newCell > currentCell)
+                {
+                    // south or east
+                    if (newCell - 1 == currentCell)
+                    {
+                        board[currentCell].status[2] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[3] = true;
+                    }
+                    else
+                    {
+                        board[currentCell].status[1] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[0] = true;
+                    }
+                }
+                else
+                {
+                    // north or west
+                    if (newCell + 1 == currentCell)
+                    {
+                        board[currentCell].status[3] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[2] = true;
+                    }
+                    else
+                    {
+                        board[currentCell].status[0] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[1] = true;
+                    }
+                }
+            }
+        }
+        GenerateDungeon();
+    }
+
+    List<int> CheckNeighbours(int cell)
+    {
+        List<int> neighbours  = new List<int>();
+
+        // Check north neighbour
+        if (cell - size.x >= 0 && !board[Mathf.FloorToInt(cell - size.x)].visited)
+        {
+            neighbours.Add(Mathf.FloorToInt(cell - size.x));
+            Debug.Log($"Northern Neighbour is visited: {board[Mathf.FloorToInt(cell - size.x)].visited}");
+        }
+
+        // Check south neighbour
+        if (cell + size.x < board.Count && !board[Mathf.FloorToInt(cell + size.x)].visited)
+        {
+            neighbours.Add(Mathf.FloorToInt(cell + size.x));
+            Debug.Log($"Southern Neighbour is visited: {board[Mathf.FloorToInt(cell + size.x)].visited}");
+        }
+
+        // Check east neighbour
+        if ((cell + 1) % size.x != 0 && !board[Mathf.FloorToInt(cell + 1)].visited)
+        {
+            neighbours.Add(Mathf.FloorToInt(cell + 1));
+            Debug.Log($"Eastern Neighbour is visited: {board[Mathf.FloorToInt(cell + 1)].visited}");
+        }
+
+        // Check west neighbour
+        if (cell % size.x != 0 && !board[Mathf.FloorToInt(cell - 1)].visited)
+        {
+            neighbours.Add(Mathf.FloorToInt(cell - 1));
+            Debug.Log($"Western Neighbour is visited: {board[Mathf.FloorToInt(cell - 1)].visited}");
+        }
+
+        return neighbours;
+    }
+}
