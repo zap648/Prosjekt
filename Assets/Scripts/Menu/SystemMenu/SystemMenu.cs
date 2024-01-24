360a4dc28c6755389963b5e51c7071e222ce9bfa
SaraYohannes <sara.yohannes@hesbynett.no>
2024-01-24 19:06:03 +0100

    game loop scene with menus
    menus are almost done: only need to fix
    
    - the text of a few buttons (on shop screen and in scene Mine8)
    - fix so that when returning from mine correct screen menu opens (PreperationScreen-group)

--- Assets/Scripts/Menu/SystemMenu/SystemMenu.cs
+++ Assets/Scripts/Menu/SystemMenu/SystemMenu.cs
@@ -4,82 +4,42 @@ using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SystemMenu : MonoBehaviour
 {
     public Button savefile_startFile;
-
-
-    // go to MainMenu
-    public void goToMainMenu_button()
-    {
-
-        /*
-        int gameobj = transform.childCount;
-        Debug.Log(gameobj);
-
-        Transform childtag = transform.GetChild(1);
-        string nameTag = childtag.tag;
-        if (nameTag == "saveFile")
-        {
-            Debug.Log("got it");
-        }
-        else if (nameTag == "grandparent")
-        {
-            Debug.Log("got parent");
-        }
-        else if (nameTag == "mainMenu")
-        {
-            Debug.Log("got the mains!");
-        }
-        else
-        {
-            Debug.Log("what sadness");
-        }
-        */
-
-        // deactivate empty objects 
-       
-        
-        // activate empty object  (main menu)
-
-    }
 
     // quit application
     public void Exit_button()
     {
         Application.Quit();
     }
-
-    // go to optionMenu
-    public void goToOptionMenu_button()
+    public void startGame_newGame_button()
     {
-        // deactivate empty objects 
-        // activate empty object  (Option menu)
+        // change scene
+        SceneManager.LoadScene(1);
     }
-
-    // go to characterCreationMenu
-    public void goToCharacterCreationMenu_button()
-    {
-        // deactivate empty objects 
-        // activate empty object  (Character creation menu)
-    }
-
-    // go to continue a saved file
-    public void goToContinueSaveFileMenu_button()
-    {
-        // deactivate empty objects 
-        // activate empty object  (Continue Save File menu)
-    }
-
     // start game file 
     public void startGameFile_button()
     {
+        // check if we have a save file, if we do- load it
+        // apply the saved file to the scene
+        // load Home Scene
+        // startGame_newGame_button();
+
         // this is currently not available
         savefile_startFile.interactable = false;
-
-        // deactivate all menues
-        // load up GameScene
-        // dismiss SystemMenuScene
+    }
+    public void goToMine()
+    {
+        // loads scene with Mine gameplay
+        SceneManager.LoadScene(2);
+    }
+    // in the mine, able to return to gameloopscene to continue loop
+    public void goToPrep()
+    {
+        // loads scene in preperation menu for after-work
+        SceneManager.LoadScene(1);
     }
 }
