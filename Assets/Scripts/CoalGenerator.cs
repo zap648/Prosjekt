3fedff0f4dd0057568a34901bbc20d250df57898
Kenneth Massao Straumsheim <kmstraumsheim@gmail.com>
2024-02-19 13:35:47 +0100

    Rooms spawn properly, coal gen turned off
    The room generation now spawns properly.
    OBS! Currently an error where the rooms don't connect properly.

--- Assets/Scripts/CoalGenerator.cs
+++ Assets/Scripts/CoalGenerator.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using UnityEngine;
-using static CaveGenerator;
 
 public class CoalGenerator : MonoBehaviour
 {
     [Tooltip("Cave Generation Script")]
-    [SerializeField] CaveGenerator caveGenerator;
+    [SerializeField] GruveGenerator gruveGenerator;
 
     [Tooltip("Average coal per room")]
     [SerializeField] int coalConsentration;
@@ -20,39 +19,39 @@ public class CoalGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        caveGenerator = GetComponent<CaveGenerator>();
+        gruveGenerator = GetComponent<GruveGenerator>();
         GenerateCoal();
     }
 
     void GenerateCoal()
     {
-        // Marks which rooms should have coal
-        for (int i = 0; i < caveGenerator.board.Count; i++)
-        {
-            // If the current cell is the first cell, don't spawn coal in it
-            if (caveGenerator.board[i] == caveGenerator.board.First())
-                continue;
+        //// Marks which rooms should have coal
+        //for (int i = 0; i < gruveGenerator.queue.Count; i++)
+        //{
+        //    // If the current cell is the first cell, don't spawn coal in it
+        //    if (gruveGenerator.queue[i] == gruveGenerator.queue.First())
+        //        continue;
 
-            // Let's say around half of the room gets coal
-            if (Random.value <= 0.50f && caveGenerator.board[i].visited)
-            {
-                caveGenerator.board[i].coal = true;
-            }
-        }
+        //    // Let's say around half of the room gets coal
+        //    if (Random.value <= 0.50f && gruveGenerator.queue[i].visited)
+        //    {
+        //        gruveGenerator.queue[i].coal = true;
+        //    }
+        //}
 
-        // Instantiates the coal
-        for (int i = 0; i < caveGenerator.size.x; i++)
-        {
-            for (int j = 0; j < caveGenerator.size.y; j++)
-            {
-                Cell currentCell = caveGenerator.board[Mathf.FloorToInt(i + j * caveGenerator.size.x)];
-                if (currentCell.coal)
-                {
-                    GameObject newCoal = Instantiate(coal, new Vector3(i * caveGenerator.offset.x + Random.Range(-caveGenerator.size.x, caveGenerator.size.x), 0, -j * caveGenerator.offset.y + Random.Range(-caveGenerator.size.y, caveGenerator.size.y)), Quaternion.Euler(0.0f, 0.0f, 0.0f), transform);
-                    coals.Add(newCoal);
-                    newCoal.name = $"Coal in {i}-{j}";
-                }
-            }
-        }
+        //// Instantiates the coal
+        //for (int i = 0; i < gruveGenerator.size.x; i++)
+        //{
+        //    for (int j = 0; j < gruveGenerator.size.y; j++)
+        //    {
+        //        Cell currentCell = gruveGenerator.queue[Mathf.FloorToInt(i + j * gruveGenerator.size.x)];
+        //        if (currentCell.coal)
+        //        {
+        //            GameObject newCoal = Instantiate(coal, new Vector3(i * gruveGenerator.offset.x + Random.Range(-gruveGenerator.size.x, gruveGenerator.size.x), 0, -j * gruveGenerator.offset.y + Random.Range(-gruveGenerator.size.y, gruveGenerator.size.y)), Quaternion.Euler(0.0f, 0.0f, 0.0f), transform);
+        //            coals.Add(newCoal);
+        //            newCoal.name = $"Coal in {i}-{j}";
+        //        }
+        //    }
+        //}
     }
 }
--- Assets/Scripts/GruveGenerator.cs
+++ Assets/Scripts/GruveGenerator.cs
@@ -4,18 +4,18 @@ using System.Linq;
 using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 
+public class Cell
+{
+    public Cell() { }
+    public Cell(int xCoordinate, int yCoordinate) { coordinates = new int[2] { xCoordinate, yCoordinate }; }
+
+    public int[] coordinates = new int[2];
+    public bool[] neighbour = new bool[4]; // 0 - up, 1 - right, 2 - down, 3 - left
+    public bool coal = false;
+}
+
 public class GruveGenerator : MonoBehaviour
 {
-    public class Cell
-    {
-        public Cell() { }
-        public Cell(int xCoordinate, int yCoordinate) {  coordinates = new int[2] { xCoordinate, yCoordinate }; }
-
-        public int[] coordinates = new int[2];
-        public bool[] neighbour = new bool[4]; // 0 - up, 1 - right, 2 - down, 3 - left
-        public bool coal = false;
-    }
-
     public GameObject room;
     public Vector2 offset;
     public List<Cell> queue;
@@ -36,35 +36,35 @@ public class GruveGenerator : MonoBehaviour
         var newRoom = Instantiate(room, new Vector3(0, 0, 0), Quaternion.Euler(90.0f, 0.0f, 0.0f), transform).GetComponent<RoomBehaviour>();
         newRoom.UpdateRoom(currentCell.neighbour);
 
-        newRoom.name += $"Room 0-0";
+        newRoom.name = $"Room 0-0";
 
-        while (iterations < 100)
+        while (iterations < 10 && iterations < queue.Count())
         {
             // To create neighbours
             for (int i = 0; i < 4; i++)
             {
-                if (Random.Range(0, 100) < 5)
+                if (Random.Range(0, 100) < 50)
                 {
                     queue[iterations].neighbour[i] = true;
 
                     if (i == 0) // If the neighbour is above (0 - up)
                     {
-                        queue.Add(new Cell(queue[iterations].coordinates[0], queue[iterations].coordinates[0] + 1));
+                        queue.Add(new Cell(queue[iterations].coordinates[0], queue[iterations].coordinates[1] + 1));
                         queue.Last().neighbour[2] = true;
                     }
                     else if (i == 1) // If the neighbour is rightside (1 - right)
                     {
-                        queue.Add(new Cell(queue[iterations].coordinates[0] + 1, queue[iterations].coordinates[0]));
+                        queue.Add(new Cell(queue[iterations].coordinates[0] + 1, queue[iterations].coordinates[1]));
                         queue.Last().neighbour[3] = true;
                     }
                     else if (i == 2) // if the neighbour is below (2 - down)
                     {
-                        queue.Add(new Cell(queue[iterations].coordinates[0], queue[iterations].coordinates[0] - 1));
+                        queue.Add(new Cell(queue[iterations].coordinates[0], queue[iterations].coordinates[1] - 1));
                         queue.Last().neighbour[0] = true;
                     }
                     else if (i == 3) // if the neighbour is leftside (3 - left)
                     {
-                        queue.Add(new Cell(queue[iterations].coordinates[0] - 1, queue[iterations].coordinates[0]));
+                        queue.Add(new Cell(queue[iterations].coordinates[0] - 1, queue[iterations].coordinates[1]));
                         queue.Last().neighbour[1] = true;
                     }
 
@@ -73,7 +73,7 @@ public class GruveGenerator : MonoBehaviour
                     newRoom = Instantiate(room, new Vector3(currentCell.coordinates[0] * offset.x, 0, currentCell.coordinates[1] * offset.y), Quaternion.Euler(90.0f, 0.0f, 0.0f), transform).GetComponent<RoomBehaviour>();
                     newRoom.UpdateRoom(currentCell.neighbour);
 
-                    newRoom.name += $" {currentCell.coordinates[0]}-{currentCell.coordinates[1]}";
+                    newRoom.name = $"Room {currentCell.coordinates[0]} - {currentCell.coordinates[1]}";
                 }
             }
             iterations++;
