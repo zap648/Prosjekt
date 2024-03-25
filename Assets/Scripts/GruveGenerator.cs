b15141e2f004e8b9da9e8457acee31a738b7e259
Kenneth Massao Straumsheim <kmstraumsheim@gmail.com>
2024-03-25 21:00:39 +0100

    Floors now accessable
    Created rooms with holes in the middle to allow players to use elevators.

--- Assets/Scripts/GruveGenerator.cs
+++ Assets/Scripts/GruveGenerator.cs
@@ -10,22 +10,30 @@ public class Cell
 
     public int[] coordinates = new int[3];
     public bool[] neighbour = new bool[4]; // 0 - up, 1 - right, 2 - down, 3 - left
+    public int roomType = 0; // 0 - normal, 1 - elevator, 2 - access
     public bool elevator = false;
     public bool coal = false;
 }
 
 public class GruveGenerator : MonoBehaviour
 {
+    [Header("Room Prefabs")]
     public GameObject roomPreFab;
-    public GameObject spawnPreFab;
-    public GameObject playerPreFab;
-    public GameObject player;
+    public GameObject elevRoomPreFab;
+    public GameObject accRoomPreFab;
+
+    [Header("Room Info")]
     public int maxRooms;
     public int minRooms;
     public int neighbourChance;
     public int floors;
     public Vector3 offset;
     public List<Cell> queue;
+
+    [Header("Player")]
+    public GameObject playerPreFab;
+    public GameObject player;
+
 
     private RoomBehaviour spawnRoom;
 
@@ -58,9 +66,16 @@ public class GruveGenerator : MonoBehaviour
                 NewFloor(queue.Last(), i);
             }
 
+            int whileCheck = 0;
             while (queue.Count() - 1 < maxRooms + maxRooms * i)
             {
                 SetupNeighbours(queue.Last(), i);
+
+                if (whileCheck > 100)
+                {
+                    Debug.Log("Endless loop detected! Breaking loop!");
+                    break;
+                }
             }
 
             if (queue.Count() < minRooms + minRooms * i)
@@ -69,7 +84,7 @@ public class GruveGenerator : MonoBehaviour
             }
         }
 
-        MergeCells();
+        //MergeCells();
     }
 
     void SetupNeighbours(Cell cell, int floor)
@@ -83,23 +98,51 @@ public class GruveGenerator : MonoBehaviour
 
                 if (i == 0) // If the neighbour is above (0 - up)
                 {
-                    queue.Add(new Cell(cell.coordinates[0], cell.coordinates[1] + 1, floor));
-                    queue.Last().neighbour[2] = true;
+                    if (!CheckRoom(new int[3] { cell.coordinates[0], cell.coordinates[1] + 1, floor }))
+                    {
+                        queue.Add(new Cell(cell.coordinates[0], cell.coordinates[1] + 1, floor));
+                        queue.Last().neighbour[2] = true;
+                    }
+                    else
+                    {
+                        FindRoom(new int[3] { cell.coordinates[0], cell.coordinates[1] + 1, floor }).neighbour[2] = true;
+                    }
                 }
                 else if (i == 1) // If the neighbour is rightside (1 - right)
                 {
-                    queue.Add(new Cell(cell.coordinates[0] + 1, cell.coordinates[1], floor));
-                    queue.Last().neighbour[3] = true;
+                    if (!CheckRoom(new int[3] { cell.coordinates[0] + 1, cell.coordinates[1], floor }))
+                    {
+                        queue.Add(new Cell(cell.coordinates[0] + 1, cell.coordinates[1], floor));
+                        queue.Last().neighbour[3] = true;
+                    }
+                    else
+                    {
+                        FindRoom(new int[3] { cell.coordinates[0] + 1, cell.coordinates[1], floor }).neighbour[3] = true;
+                    }
                 }
                 else if (i == 2) // if the neighbour is below (2 - down)
                 {
-                    queue.Add(new Cell(cell.coordinates[0], cell.coordinates[1] - 1, floor));
-                    queue.Last().neighbour[0] = true;
+                    if (!CheckRoom(new int[3] { cell.coordinates[0], cell.coordinates[1] - 1, floor }))
+                    {
+                        queue.Add(new Cell(cell.coordinates[0], cell.coordinates[1] - 1, floor));
+                        queue.Last().neighbour[0] = true;
+                    }
+                    else
+                    {
+                        FindRoom(new int[3] { cell.coordinates[0], cell.coordinates[1] - 1, floor }).neighbour[0] = true;
+                    }
                 }
                 else if (i == 3) // if the neighbour is leftside (3 - left)
                 {
-                    queue.Add(new Cell(cell.coordinates[0] - 1, cell.coordinates[1], floor));
-                    queue.Last().neighbour[1] = true;
+                    if (!CheckRoom(new int[3] { cell.coordinates[0] - 1, cell.coordinates[1], floor }))
+                    {
+                        queue.Add(new Cell(cell.coordinates[0] - 1, cell.coordinates[1], floor));
+                        queue.Last().neighbour[1] = true;
+                    }
+                    else
+                    {
+                        FindRoom(new int[3] { cell.coordinates[0] - 1, cell.coordinates[1], floor }).neighbour[1] = true;
+                    }
                 }
             }
         }
@@ -108,7 +151,8 @@ public class GruveGenerator : MonoBehaviour
     void NewFloor(Cell cell, int toFloor)
     {
         queue.Add(new Cell(cell.coordinates[0], cell.coordinates[1], toFloor));
-        queue.Last().elevator = true;
+        queue.Last().roomType = 1;
+        FindRoom(new int[] { queue.Last().coordinates[0], queue.Last().coordinates[1], queue.Last().coordinates[2] - 1 }).roomType = 2;
     }
 
     void MergeCells()
@@ -145,7 +189,7 @@ public class GruveGenerator : MonoBehaviour
 
     void CreateRooms()
     {
-        RoomBehaviour newRoom = Instantiate(spawnPreFab, new Vector3(queue[0].coordinates[0] * offset.x, queue[0].coordinates[2] * (offset.z * (-1)), queue[0].coordinates[1] * offset.y), Quaternion.Euler(0.0f, 0.0f, 0.0f), transform).GetComponent<RoomBehaviour>();
+        RoomBehaviour newRoom = Instantiate(elevRoomPreFab, new Vector3(queue[0].coordinates[0] * offset.x, queue[0].coordinates[2] * (offset.z * (-1)), queue[0].coordinates[1] * offset.y), Quaternion.Euler(0.0f, 0.0f, 0.0f), transform).GetComponent<RoomBehaviour>();
         newRoom.UpdateRoom(queue[0].neighbour);
 
         newRoom.name = $"Spawn Room";
@@ -153,15 +197,19 @@ public class GruveGenerator : MonoBehaviour
 
         for (int i = 1; i < queue.Count(); i++)
         {
-            if (!queue[i].elevator)
+            if (queue[i].roomType == 0)
             {
                 newRoom = Instantiate(roomPreFab, new Vector3(queue[i].coordinates[0] * offset.x, queue[i].coordinates[2] * (offset.z * (-1)), queue[i].coordinates[1] * offset.y), Quaternion.Euler(0.0f, 0.0f, 0.0f), transform).GetComponent<RoomBehaviour>();
             }
-            else
+            else if (queue[i].roomType == 1)
             {
-                newRoom = Instantiate(spawnPreFab, new Vector3(queue[i].coordinates[0] * offset.x, queue[i].coordinates[2] * (offset.z * (-1)), queue[i].coordinates[1] * offset.y), Quaternion.Euler(0.0f, 0.0f, 0.0f), transform).GetComponent<RoomBehaviour>();
+                newRoom = Instantiate(elevRoomPreFab, new Vector3(queue[i].coordinates[0] * offset.x, queue[i].coordinates[2] * (offset.z * (-1)), queue[i].coordinates[1] * offset.y), Quaternion.Euler(0.0f, 0.0f, 0.0f), transform).GetComponent<RoomBehaviour>();
                 newRoom.gameObject.GetComponentInChildren<GruveElevator>().transform.position = new Vector3(newRoom.gameObject.GetComponentInChildren<GruveElevator>().transform.position.x, newRoom.gameObject.GetComponentInChildren<GruveElevator>().topHeight, newRoom.gameObject.GetComponentInChildren<GruveElevator>().transform.position.z);
                 newRoom.gameObject.GetComponentInChildren<GruveElevator>().atTop = true;
+            }
+            else if (queue[i].roomType == 2)
+            {
+                newRoom = Instantiate(accRoomPreFab, new Vector3(queue[i].coordinates[0] * offset.x, queue[i].coordinates[2] * (offset.z * (-1)), queue[i].coordinates[1] * offset.y), Quaternion.Euler(0.0f, 0.0f, 0.0f), transform).GetComponent<RoomBehaviour>();
             }
             newRoom.UpdateRoom(queue[i].neighbour);
 
@@ -171,31 +219,58 @@ public class GruveGenerator : MonoBehaviour
 
     bool CheckRoom(int[] coordinates)
     {
+        if (coordinates.Length != 3)
+        {
+            Debug.Log($"Coordinates are only {coordinates.Length} cells large, the function needs 3 cells");
+        }
+
+        bool roomExists = false;
         foreach (Cell room in queue)
         {
-            if (room.coordinates == coordinates)
+            if (room.coordinates[0] == coordinates[0] &&
+                room.coordinates[1] == coordinates[1] &&
+                room.coordinates[2] == coordinates[2])
             {
-                return true;
+                roomExists = true;
+                break;
             }
         }
 
-        return false;
+        if (roomExists)
+        {
+            Debug.Log($"The room exists");
+        }
+        else
+        {
+            Debug.Log($"The room doesn't exist");
+        }
+
+        return roomExists;
     }
 
     Cell FindRoom(int[] coordinates)
     {
+        if (coordinates.Length != 3)
+        {
+            Debug.Log($"Coordinates are only {coordinates.Length} cells large, the function needs 3 cells");
+        }
+
+        Cell room = null;
+
         if (CheckRoom(coordinates))
         {
-            foreach (Cell room in queue)
+            foreach (Cell scan in queue)
             {
-                if (room.coordinates == coordinates)
+                if (scan.coordinates[0] == coordinates[0] &&
+                    scan.coordinates[1] == coordinates[1] &&
+                    scan.coordinates[2] == coordinates[2])
                 {
-                    return room;
+                    room = scan;
+                    break;
                 }
             }
         }
 
-        Debug.Log($"Room {coordinates[0]}-{coordinates[1]} not found");
-        return null;
+        return room;
     }
 }
