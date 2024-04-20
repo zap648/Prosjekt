4bd6fe0e1dd949fac452d51a8949fb6df5aefb78
SaraYohannes <sara.yohannes@hesbynett.no>
2024-04-20 17:39:02 +0200

    basic saving function
    Using a singleton to save what is in a struct to a binary file, then a loading function reading from that same file

---
+++ Assets/Scripts/SaveLoad/SO_saveload.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "new save and loader", menuName = "Testing new things!")]
+public class SO_saveload : ScriptableObject
+{
+    public string SO_name = "Darling";
+
+}
